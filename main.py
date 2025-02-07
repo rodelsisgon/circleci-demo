86705b00c932400c8249fcedf883b90a9a3fa946
rodelsisgon <rodel.sisgon@ft.com>
2025-02-07 06:38:53 +0000

    Updated (test) python files

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
 def addme(a,b):
     return a + b
+
+def SayHello():
+    print("sup world from srcmake")
+
+if __name__ == '__main__':
+    SayHello()
--- test_main.py
+++ test_main.py
@@ -2,6 +2,10 @@ import pytest
 from main import addme
 
 def test_addme():
-    assert addme(1,1)==2
-    assert addme(10,11)==21
-    assert addme(-1,20)==19
+    assert addme(1,1) == 2
+    assert addme(10,11) == 21
+    assert addme(-1,20) == 19
+    print("addme Function works correctly")
+
+if __name__ == '__main__':
+    test_addme()
